fcacc13c57beaa1623ea93c1135daf3d3e7e486d
josephshim02 <josephshim02@gmail.com>
2025-10-06 17:23:59 +1100

    Ep 6 props declare with props: {} in components Can conditionally apply classes withing template: `` with `:class="{"class name": true,...}"`

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,15 +1,28 @@
 export default {
-          template: `
-            <button class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600" :disabled="processing">
+  props: {
+    type: {
+      type: String,
+      default: 'primary'
+    },
+
+    processing: {
+      type: Boolean,
+      default: false
+    }
+  },
+  template: `
+            <button 
+            :class="{
+              'px-4 py-2 text-white rounded': true,
+              'bg-blue-500 hover:bg-blue-600': type === 'primary',
+              'bg-gray-500 text-white hover:bg-gray-600': type === 'secondary',
+              'is-loading': processing
+            }"
+            :disabled="processing">
               <slot />
             </button>
           `,
-          mounted(){
-            console.log('button mounted')
-          },
-          data () {
-            return {
-              processing: false
-            }
-          },
-        }
+  mounted() {
+    console.log('button mounted')
+  }
+}
