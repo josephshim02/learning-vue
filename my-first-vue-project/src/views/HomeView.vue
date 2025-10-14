d597fa450c899087d599acc0c2b99274b12712cd
josephshim02 <josephshim02@gmail.com>
2025-10-14 13:00:48 +1100

    Introduced to composition API
    Allows us to cut down on code bloat, no need to register components, simply add setup to the script tag.
    
    Then you can define dynamic variables with ref. echo them with {{variable}} in html but modify them with variable.value in script tag

--- my-first-vue-project/src/views/HomeView.vue
+++ my-first-vue-project/src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+
+
+
+let message = ref("Hello world");
+setTimeout(() => {
+  message.value = "new message";
+}, 2000);
+
+
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
   </main>
+  <p>{{message}}</p>
+  <input type="text" v-model="message">
 </template>
