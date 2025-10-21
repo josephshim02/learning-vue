5cf241deefafb1ccfae5fdc2a136346269cf359d
josephshim02 <josephshim02@gmail.com>
2025-10-21 12:53:44 +1100

    Ep 25 How to use pinia
    The main advantage seems to be vue devtools but I don't even have access to those in the laravel project

---
+++ my-first-vue-project/src/stores/useCounterStore.js
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+
+export let useCounterStore = defineStore("counter", {
+  state() {
+    return {
+      count: 2
+    }
+  },
+  actions: {
+    increment() {
+      if (this.count < 10) {
+        this.count++;
+      }
+    }
+  },
+  getters: {
+    remaining() {
+      return 10 - this.count;
+    }
+  }
+
+})
--- my-first-vue-project/src/views/CounterView.vue
+++ my-first-vue-project/src/views/CounterView.vue
@@ -1,10 +1,13 @@
 <script setup>
-import { counter } from '@/stores/counterStore';
+//import { counter } from '@/stores/counterStore';
+import { useCounterStore } from '@/stores/useCounterStore';
+
+let counter = useCounterStore();
 </script>
 
 
 <template>
   <p>I am counter view</p>
   <p>{{counter.count}}</p>
-  <button @click="counter.increment()">Increment</button>
+  <button @click="counter.increment" :disabled="(counter.remaining == 0)">Increment ({{counter.remaining}} remaining)</button>
 </template>
