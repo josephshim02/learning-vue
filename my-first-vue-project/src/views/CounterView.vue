878ffe6854fab73a68615adac57d7418bb2942f8
josephshim02 <josephshim02@gmail.com>
2025-10-21 12:35:20 +1100

    Ep 24 stores continued
    Official vue documentation discourages directly mutating stores. Instead add a mutation method to the store and call that method

---
+++ my-first-vue-project/src/views/CounterView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { counter } from '@/stores/counterStore';
+</script>
+
+
+<template>
+  <p>I am counter view</p>
+  <p>{{counter.count}}</p>
+  <button @click="counter.increment()">Increment</button>
+</template>
