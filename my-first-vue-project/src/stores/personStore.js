d73dc8bfc35b90be6e725f7c59f4bc71cf839d51
josephshim02 <josephshim02@gmail.com>
2025-10-21 12:16:59 +1100

    Ep 23

---
+++ my-first-vue-project/src/stores/personStore.js
@@ -0,0 +1,7 @@
+import { reactive } from "vue";
+
+export let person = reactive({
+  name: "joseph",
+  age: 22,
+
+})
--- my-first-vue-project/src/views/ContactView.vue
+++ my-first-vue-project/src/views/ContactView.vue
@@ -1,7 +1,44 @@
+<script setup>
+import test from '@/components/test.vue';
+import info from '@/components/info.vue';
+import {person} from '@/stores/personStore.js'
+import { ref, onMounted,onBeforeUnmount, provide } from 'vue';
+const blah = ref(0)
+const cardRef = ref(null);
+const cardWidth = ref(200);
+let resizeObserver = null;
+
+onMounted(() => {
+  if (cardRef.value) {
+    resizeObserver = new ResizeObserver(entries => {
+      for (const entry of entries) {
+        const width = entry.contentRect.width
+        if (width > 0) cardWidth.value = width
+      }
+    })
+    resizeObserver.observe(cardRef.value)
+  };
+})
+
+onBeforeUnmount(() => {
+  if (resizeObserver && cardRef.value) {
+    resizeObserver.unobserve(cardRef.value)
+    resizeObserver.disconnect()
+  }
+});
+
+
+provide('cardWidth', cardWidth);
+</script>
+
 <template>
   <div class="contact">
     <h1>This is a contact page</h1>
+    <info/>
   </div>
+  <p> {{person.name}}</p>
+  <p> {{person.age}}</p>
+  <button @click="person.name='shoeyan'">Change name</button>
 </template>
 
 <style>
